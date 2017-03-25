cb4f245aecdbcc78ca5db6fadf543bd20aaf036d
scfoxcode <scfoxcode@hotmail.com>
2017-03-25 12:39:16 +0000

    Target Painter Script Works
    Use to laze targets, co-ordinates are converted to gps copy paste format

--- source/targetPainter.cs
+++ source/targetPainter.cs
@@ -15,8 +15,6 @@ BLOCKS:
 -
 \*---------------------------------------------------------------------------*/ 
 
-
-
 #region pre-script
 using System;
 using System.Text;
@@ -39,20 +37,73 @@ namespace IngameScript
     {
 #endregion
         //To put your code in a PB copy from this comment...
-        public void Init()
+        // Find working block with name
+        public Type FindBlock<Type>(string name) where Type: class, IMyTerminalBlock
         {
- 
+            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+            GridTerminalSystem.SearchBlocksOfName(name, blocks); 
+            for(var i=0; i<blocks.Count; i++)
+            {
+                if(blocks[i].IsWorking)
+                {
+                    return blocks[i] as Type;
+                }
+            } 
+            return null;
         }
- 
-        public void Save()
+
+        // Save co-ordinates
+        public void SaveCoords(MyDetectedEntityInfo data, IMyTextPanel display)
         {
- 
+            string result = "GPS:PAINTED:";
+            Vector3D? pos = data.HitPosition;
+            if(pos == null)
+            {
+                pos = data.BoundingBox.Center;
+            }
+            string position = Math.Round(pos.Value.X, 2).ToString() + ":" +
+                              Math.Round(pos.Value.Y, 2) + ":" + 
+                              Math.Round(pos.Value.Z, 2) + ":";
+
+            result += position + "\n";
+            Storage += result;
+
+            display.WritePublicText(Storage, false);
         }
- 
+
+        // Start program
         public void Main(string argument)
         {
-            Init();
-        }
+            int SCAN_DISTANCE = 1000;    // Distance we can scan
+            int SCAN_DISTANCE_MIN = 100; // Smallest acceptable distance
+
+            // Find a working target painter
+            IMyCameraBlock targetPainter = FindBlock<IMyCameraBlock>("targetPainter_Camera");
+            if(targetPainter == null)
+            {
+                return; // Couldn't find target painter
+            }
+            targetPainter.EnableRaycast = true;
+
+            // Find a working display
+            IMyTextPanel panel = FindBlock<IMyTextPanel>("targetPainter_Display");
+            if(panel == null)
+            {
+                return; // Couldn't find a display
+            }
+
+            // Try to scan at max distance but settle for smaller value 
+            MyDetectedEntityInfo data; 
+            while(SCAN_DISTANCE > SCAN_DISTANCE_MIN)
+            {
+                if(targetPainter.CanScan(SCAN_DISTANCE))
+                {
+                    data = targetPainter.Raycast(SCAN_DISTANCE, 0, 0);
+                    SaveCoords(data, panel);
+                    return;
+                }
+            }
+        }  
         //to this comment.
 #region post-script
     }
