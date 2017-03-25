e015cffa0ecaa338903d6b3ecebccef24938d62e
scfoxcode <scfoxcode@hotmail.com>
2017-03-25 09:29:46 +0000

    Added template and empty targetPainter
    Saved a template script to use as an easy copy paste when building new
    scripts.
    
    Created the targetPainter file. This script will aquire gps co-ordinates
    of targets using a camera and save the data to an lcd panel. It is empty
    for now

---
+++ source/targetPainter.cs
@@ -0,0 +1,60 @@
+﻿// Stephen Surtees Space Engineers Scripting Fun
+
+// Setup guide at
+// http://www.spaceengineerswiki.com/Visual_Studio_Setup_Guide
+
+/*---------------------------------------------------------------------------*\
+                              TARGET PAINTER SCRIPT
+
+DESCRIPTION: Obtains GPS co-ordinates of a target using camera raycast.
+Co-ordinates are saved onto LCD screen for later use
+
+BLOCKS:
+- Camera:    Must be named "targetPainter_Camera"
+- LCD Panel: Must be named "targetPainter_Display"
+-
+\*---------------------------------------------------------------------------*/ 
+
+
+
+#region pre-script
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using VRageMath;
+using VRage.Game;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.Collections;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+ 
+namespace IngameScript
+{
+    public class targetPainter : MyGridProgram
+    {
+#endregion
+        //To put your code in a PB copy from this comment...
+        public void Init()
+        {
+ 
+        }
+ 
+        public void Save()
+        {
+ 
+        }
+ 
+        public void Main(string argument)
+        {
+            Init();
+        }
+        //to this comment.
+#region post-script
+    }
+}
+#endregion
---
+++ source/templateScript.cs
@@ -0,0 +1,59 @@
+﻿// Stephen Surtees Space Engineers Scripting Fun
+
+// Setup guide at
+// http://www.spaceengineerswiki.com/Visual_Studio_Setup_Guide
+
+/*---------------------------------------------------------------------------*\
+                                NAME GOES HERE
+
+DESCRIPTION:
+
+BLOCKS:
+-
+-
+-
+\*---------------------------------------------------------------------------*/ 
+
+
+#region pre-script
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using VRageMath;
+using VRage.Game;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.Collections;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+ 
+namespace IngameScript
+{
+    public class Program : MyGridProgram
+    {
+#endregion
+        //To put your code in a PB copy from this comment...
+        public void Init() // Init rather than public due to naming issues
+        {
+ 
+        }
+ 
+        public void Save()
+        {
+ 
+        }
+ 
+        public void Main(string argument)
+        {
+            Init();
+            Storage = "fun";
+        }
+        //to this comment.
+#region post-script
+    }
+}
+#endregion
